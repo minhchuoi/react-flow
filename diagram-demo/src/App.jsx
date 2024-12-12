664a3428c76527c238a3014d6142e67e761e1cee
ZaffranLe <biprono12@gmail.com>
2024-12-13 01:04:33 +0700

    feature: add react flow

--- diagram-demo/src/App.jsx
+++ diagram-demo/src/App.jsx
@@ -2,13 +2,15 @@ import { Route, Routes } from "react-router";
 import Dashboard from "./pages/dashboard";
 import Login from "./pages/login";
 import Register from "./pages/register";
+import Diagram from "./pages/diagram";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="dashboard" element={<Dashboard />} />
       <Route path="/" element={<Login />} />
-      <Route path="register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/diagram" element={<Diagram />} />
     </Routes>
   );
 };
